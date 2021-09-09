3c0d18447d6e6cf8e5418dbc0e3457bca124ec56
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2021-09-10 00:21:39 +0300

    progress

--- Egron/person.js
+++ Egron/person.js
@@ -2,11 +2,41 @@ function person(fName, lName, ID, City, birthDate, parentID) {
     this.fName = fName;
     this.lName = lName;
     this.ID = ID;
-    this.City = City;
+    this.city = city;
     this.birthDate = birthDate;
     this.parentID = parentID;
 }
 
-//let pers = new person('Daniel', 'Kalfa' , '318448032', 'Jerusalem', '07/06/1997', '318');
 let json = require('./data.json');
-console.log(json);
+json = json.persons;
+
+function searchByCity(city) {
+    json.forEach(element => {
+        if (element.city == city) {
+            toString(element);    
+        }
+    });
+}
+function searchByFName(name) {
+    json.forEach(element => {
+        if (element.fName.toLowerCase().includes(name.toLowerCase())) {
+            toString(element);    
+        }
+    });
+}
+function searchByLName(name) {
+    json.forEach(element => {
+        if (element.lName.toLowerCase().includes(name.toLowerCase())) {
+            toString(element);    
+        }
+    });
+}
+
+function toString(person) {
+    console.log(`${person.fName} ${person.lName}
+    ID: ${person.ID}
+    City: ${person.city}
+    Birth Date: ${person.birthDate}
+    Parent ID: ${person.parentID}`);
+}
+searchByLName('Da');
