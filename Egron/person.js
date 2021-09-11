8c849f4dfcf416d1bed5f4d6147d7b286eba98a3
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2021-09-12 01:14:14 +0300

    update person.js, correct some data on data.json

--- Egron/person.js
+++ Egron/person.js
@@ -1,3 +1,10 @@
+//TODO: 1.check how to work with chrome running time.
+//      2.how to integrate with other js files. 
+//      3.how to use DDMMYYYY Date() format or how to convert to this format easily.
+//      4.where the validation of the data is happaning and where to write that code.
+//      5.how to write the Person() object properly and how to require some fields to be a must.
+
+//Object of person
 function person(fName, lName, ID, City, birthDate, parentID) {
     this.fName = fName;
     this.lName = lName;
@@ -43,6 +50,43 @@ function searchByName(name) {
         }
     });
 }
+//get age from user andsearch for all persons who are in this age or older.
+function searchByAge(age) {
+    let currentDate = new Date();
+    json.forEach(element => {
+        let userBirthDate = new Date(correctDateFormat(element.birthDate));
+        if (currentDate.getFullYear() - userBirthDate.getFullYear() > age) {
+            toString(element);
+        }
+    });
+}
+//search for all the persons who born in even
+//OR has at least two childs
+//OR that there first name is palindrom.
+function searchBySomething() {
+    json.forEach(element => {
+        let userBirthDate = new Date(correctDateFormat(element.birthDate));
+        if ((userBirthDate.getMonth()+1) % 2 == 0 || howManyChilds(element.ID) >= 2) {
+            toString(element);
+        }
+    });
+}
+//return how many childs does person have.
+function howManyChilds(ID) {
+    let counter = 0;
+    json.forEach(element => {
+        if (element.parentID == ID) {
+            counter++;
+        }
+    });
+    return counter;
+}
+//correct the date format from DDMMYYYY to MMDDYYYY
+//(couse js stupid)
+function correctDateFormat(date) {
+    let dateParts = date.split("/");
+    return `${dateParts[1]}/${++dateParts[0]}/${dateParts[2]}`; 
+}
 //Print the person.
 function toString(person) {
     console.log(`${person.fName} ${person.lName}
@@ -51,4 +95,4 @@ function toString(person) {
     Birth Date: ${person.birthDate}
     Parent ID: ${person.parentID}`);
 }
-searchByParentID(undefined);
+searchBySomething();
