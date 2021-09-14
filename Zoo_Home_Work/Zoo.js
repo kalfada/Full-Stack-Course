2148283d5342650376d7fcaa63fcf38e2b43fff4
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2021-09-14 17:08:36 +0300

    Update Zoo.js

--- Zoo_Home_Work/Zoo.js
+++ Zoo_Home_Work/Zoo.js
@@ -1,19 +1,10 @@
 let txt1 = "Dog12, CAT3, LiOn7, DolphiN11, fish6"
 let txt2 = "PIG17, bear29, BiRd8, SNAKE39, donkey14"
-let txt = txt1 + ", " + txt2;
-let animalArr = txt.split(",");
-
-animalArr.forEach(element => {
-    if (element[0] == " ") {
-        element.split("").splice(0, 1).join();
-    }
-});
-
+let animalArr = (txt1 + ", " + txt2).split(", ");
 let choose = 0;
 
-console.log(animalArr);
 while (choose != 6) {
-
+    
     choose = prompt(`Welcome to thee Data Base.
     Please choose from one of the options:
     [1] Search by animal code
@@ -22,24 +13,24 @@ while (choose != 6) {
     [4] Delete animal
     [5] See all the animals
     [6] Exit`);
-
+    
     switch (choose) {
-        case 1:
-            searchByAnimalCode(prompt(`Enter animal code please:`));
+        case 1: searchByAnimalCode(11);
             break;
         case 2:
             searchByAnimalCode(prompt(`Enter animal name please:`));
             break;
         case 3:
-
+            addAnimal();
             break;
         case 4:
-
+            deleteAnimal();
             break;
         case 5:
-
+            printAnimals();
             break;
         case 6:
+
             break;
         default:
             //alert(`Please choose one of the options above!`);
@@ -48,9 +39,24 @@ while (choose != 6) {
 }
 
 function searchByAnimalCode(code) {
-
+    for (let index = 0; index < animalArr.length; index++) {
+        if(animalArr[index].includes(code))
+        alert(animalArr[index].replace(code,''));        
+    }
 }
 
 function searchByAnimalName(params) {
+
+}
+
+function addAnimal(params) {
+
+}
+
+function deleteAnimal(params) {
+
+}
+
+function printAnimals(params) {
 
 }
