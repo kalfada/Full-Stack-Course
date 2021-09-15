5a3b0881b75a18d2860da18bb65eca3dd2be116f
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2021-09-15 17:24:52 +0300

    Update Zoo.js

--- Zoo_Home_Work/Zoo.js
+++ Zoo_Home_Work/Zoo.js
@@ -1,9 +1,12 @@
 let txt1 = "Dog12, CAT3, LiOn7, DolphiN11, fish6"
 let txt2 = "PIG17, bear29, BiRd8, SNAKE39, donkey14"
 let animalArr = (txt1 + ", " + txt2).split(", ");
+for (let index = 0; index < animalArr.length; index++) {
+    animalArr[index] = formatAnimalName(animalArr[index]);    
+}
 let choose = 0;
 
-while (choose != 6) {
+/*while (choose != 6) {
     
     choose = prompt(`Welcome to thee Data Base.
     Please choose from one of the options:
@@ -36,27 +39,68 @@ while (choose != 6) {
             //alert(`Please choose one of the options above!`);
             break;
     }
-}
+}*/
 
 function searchByAnimalCode(code) {
     for (let index = 0; index < animalArr.length; index++) {
-        if(animalArr[index].includes(code))
-        alert(animalArr[index].replace(code,''));        
+        if (code == seperateCode(animalArr[index])) {
+            return seperateName(animalArr[index]);
+        }
+    }
+    return false;
+}
+
+function searchByAnimalName(name) {
+    for (let index = 0; index < animalArr.length; index++) {
+        if (name == seperateName(animalArr[index])) {
+            return seperateCode(animalArr[index]);
+        }
+    }
+    return false;
+}
+
+function addAnimal(animal) {
+    if (!searchByAnimalName(animal) && !searchByAnimalCode(animal)) {
+        animalArr.push(animal);
     }
 }
 
-function searchByAnimalName(params) {
-
+function deleteAnimal(animal) {
+    if (searchByAnimalName(animal) && searchByAnimalCode(animal)) {
+        animalArr.push(animal);
+    }
 }
 
-function addAnimal(params) {
-
+function printAnimals() {
+    for (let index = 0; index < animalArr.length; index++) {
+        toString(animalArr[index]);        
+    }
 }
 
-function deleteAnimal(params) {
-
+function seperateName(name) {
+    let newName = '';
+    for (let index = 0; index < name.length; index++) {
+        if (isNaN(name[index])) {
+            newName += name[index];
+        }        
+    }
+    return newName;
 }
 
-function printAnimals(params) {
+function seperateCode(name) {
+    let newCode = '';
+    for (let index = 0; index < name.length; index++) {
+        if (!isNaN(name[index])) {
+            newCode += name[index];
+        }        
+    }
+    return newCode;
+}
 
+function toString(animal) {
+    console.log(`${seperateName(animal)} | ${seperateCode(animal)}`);
+}
+
+function formatAnimalName(animal) {
+    return animal.charAt(0).toUpperCase() + animal.slice(1).toLowerCase();
 }
