354dbcc9f6a8209cb29d074a2b9770e9bf085915
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2021-09-17 01:14:26 +0300

    Update Zoo.js

--- Zoo_Home_Work/Zoo.js
+++ Zoo_Home_Work/Zoo.js
@@ -1,6 +1,7 @@
 let txt1 = "Dog12, CAT3, LiOn7, DolphiN11, fish6"
 let txt2 = "PIG17, bear29, BiRd8, SNAKE39, donkey14"
 let animalArr = (txt1 + ", " + txt2).split(", ");
+const notExist = -1;
 for (let index = 0; index < animalArr.length; index++) {
     animalArr[index] = formatAnimalName(animalArr[index]);
 }
@@ -17,48 +18,57 @@ while (choose != 6) {
     [5] See all the animals
     [6] Exit`);
 
-    if (choose == 1) {
-        printAnimalName(searchAnimal(prompt('Enter animal code:')));
-    } else if (choose == 2) {
-        printAnimalCode(searchAnimal(prompt('Enter animal code:')));
-    } else if (choose == 3) {
-        addAnimal();
-    } else if (choose == 4) {
-        deleteAnimal(prompt('enter code for the animal you want to delete'));
-    } else if (choose == 5) {
-        printAnimals();
+    switch (choose) {
+        case '1': printAnimalName(searchAnimalByCode(prompt('Enter animal code:')));
+            break;
+        case '2': printAnimalCode(searchAnimalByName(prompt('Enter animal code:')));
+            break;
+        case '3': addAnimal();
+            break;
+        case '4': deleteAnimal(prompt('enter code for the animal you want to delete'));
+            break;
+        case '5': printAnimals();
+            break;
     }
-
 }
-//Search animal in the array by the name or the code of the animal
+//Search animal in the array by the name of the animal
 //and return the index of the animal.
-//if doesn't exist, return -1
-function searchAnimal(str) {
-    if (isNaN(str)) {
-        str = formatAnimalName(str);
-    }
+//if doesn't exist, return false
+function searchAnimalByName(str) {
+    str = formatAnimalName(str);
     for (let index = 0; index < animalArr.length; index++) {
-        if (str == seperateName(animalArr[index]) || str == seperateCode(animalArr[index])) {
+        if (str == seperateName(animalArr[index])) {
             return index;
         }
     }
-    return -1;
+    return notExist;
+}
+//Search animal in the array by the code of the animal
+//and return the index of the animal.
+//if doesn't exist, return false
+function searchAnimalByCode(str) {
+    for (let index = 0; index < animalArr.length; index++) {
+        if (str == seperateCode(animalArr[index])) {
+            return index;
+        }
+    }
+    return notExist;
 }
 //Get animal index and print the name of the animal
 function printAnimalName(index) {
-    index != -1 ? alert(seperateName(animalArr[index])) : alert("Animal doesn't axist");
+    index != notExist ? alert(seperateName(animalArr[index])) : alert("Animal doesn't axist");
 
 }
 //Get animal index and print the code of the animal
 function printAnimalCode(index) {
-    index != -1 ? alert(seperateCode(animalArr[index])) : alert("Animal doesn't axist");
+    index != notExist ? alert(seperateCode(animalArr[index])) : alert("Animal doesn't axist");
 }
 //Add animal to the array.
 function addAnimal() {
     let name = prompt('enter animal name:');
-    if (searchAnimal(name) == -1) {
+    if (searchAnimalByName(name) == notExist) {
         let code = prompt('enter animal code:');
-        if (searchAnimal(code) == -1) {
+        if (searchAnimalByCode(code) == notExist) {
             animalArr.push(name + code);
         } else {
             alert('animal alredy exist');
@@ -68,12 +78,14 @@ function addAnimal() {
     }
 }
 //Delete animal from the array.
-function deleteAnimal(animal) {
-    if (searchAnimal(animal) != -1) {
-        let ans = prompt('are you sure you want to delete this animal?');
+function deleteAnimal(code) {
+    if (searchAnimalByCode(code) != notExist) {
+        let ans = prompt('are you sure you want to delete this animal?y/n');
         if (ans == 'y') {
-            animalArr.splice(searchAnimal(animal), 1);
+            animalArr.splice(searchAnimalByCode(code), 1);
         }
+    } else {
+        alert('animal does not axist')
     }
 }
 //Print all the animals in the array.
