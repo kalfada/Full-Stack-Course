4bd477618304a9cc06c01b07abbe442a3409b86f
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2021-10-10 23:00:22 +0300

    Update WinterTime.js

--- Example/WinterTime.js
+++ Example/WinterTime.js
@@ -1,4 +1,4 @@
-let person = {
+/*let person = {
     name : 'Daniel',
     age : 24,
     city : "Jerusalem",
@@ -7,8 +7,37 @@ let person = {
 person.height = prompt('enter your height:');
 person.changed = person.height;
 delete person.height;
-for (const key in person) {
-    if (key == 'city') {
-        console.log(person.city);
+console.log(`the key 'city' : ${person.hasOwnProperty("city")}`);
+*/
+
+let monster = {
+    eyes: 2,
+    legs: 4,
+    hands: 2,
+    ears: 2,
+    mouth: 1
+}
+
+let num = prompt('please enter number: ');
+print(howMany(monster));
+
+
+function howMany(obj) {
+    let arr = [];
+    for (const key in obj) {
+        if (obj[key] == num) {
+            arr.push(key);
+        }
     }
+    return arr;
+}
+
+function print(arr) {
+    let msg = '';
+    for (const key of arr) {
+        msg += key + '\n';
+    }
+    alert(`Number : ${num}
+Appear : ${arr.length}
+${msg}`);
 }
