baf7a84e0d1c63fffb8b58f9a952f1e16f86586f
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2021-10-22 13:25:15 +0300

    update

--- Egron/personNew.js
+++ Egron/personNew.js
@@ -1,7 +1,20 @@
+/**
+ * @author Daniel Kalfa
+ * @copyright
+ * This program represents a mini Egron
+ * that have many objects of Person type
+ * can add and delete persons from the egron
+ * can search for persons
+ */
 
-
-
-
+/**
+ * @param {String} fName 
+ * @param {String} lName 
+ * @param {String} ID 
+ * @param {String} city 
+ * @param {String} birthDate 
+ * @param {String} parentID 
+ */
 function person(fName, lName, ID, city, birthDate, parentID) {
     this.fName = fName;
     this.lName = lName;
@@ -34,12 +47,21 @@ function person(fName, lName, ID, city, birthDate, parentID) {
 }
 
 //Initialization of the egron object which is an array of person objects.
-let egron = [];
-require('./data.json').persons.forEach(element => egron.push(new person(element.fName, element.lName, element.ID, element.city, element.birthDate, element.parentID)));
+//let egron = [];
+//require('./data.json').persons.forEach(element => egron.push(new person(element.fName, element.lName, element.ID, element.city, element.birthDate, element.parentID)));
 
-let pers = new person('daniel', 'kalfa', 318448032, 'jerusalem', '07/06/1997', '');
+//Update the data.json file
+function updateJsonFile() {
+    let json = JSON.stringify(egron);
+    let fs = require('fs');
+    fs.writeFileSync('./data.json', json, function (err) {
+            if (err)
+                throw err;
+            console.log('deleted');
+        });
+}
 
-
+//Allow the user to add person to the Egron.
 function addNewPerson() {
     let fName = prompt('Enter first name:');
     while (isNotValidWord(fName)) {
@@ -64,9 +86,22 @@ function addNewPerson() {
     egron.push(new person(fName, lName, ID, city, parentID));
 }
 
+deletePerson();
+function deletePerson() {
+    let pers = '318448032'//prompt('Enter the ID of the person you want to delete:');
+
+
+    if (searchByID(pers) != -1) {
+        // egron[searchByID(pers)].toString();
+        console.log();
+        egron.splice(searchByID(pers),1);
+        printEgron();
+        updateJsonFile();
+    }
+}
 //Search for ID in the 
 function searchByID(ID) {
-    return egron.find(element => element.ID == ID);
+    return egron.findIndex(element => element.ID == ID);
 }
 
 //Search for person by given name
@@ -81,13 +116,19 @@ function searchForChilds(parentID) {
     return egron.filter(element => element.parentID == parentID);
 }
 
-
 //Check if string is not a valid word,
 //meaning don't have any chars except from letters.
 function isNotValidWord(str) {
     return /([^A-Z])/gi.test(str);
 }
 
+//Print all the Egron.
+function printEgron() {
+    egron.forEach(element => element.toString());
+}
+
+//Allow the user to set all properties of person object
+//except from birthday.
 function setPerson(pers) {
     let choise = 0;
     while (choise != 6) {
@@ -118,35 +159,69 @@ function setPerson(pers) {
     }
 }
 
-let choise = 0;
-
-while (choise != 10) {
-    choise = prompt(`Welcome to the Egron, Please choose what you want to do:
-    [1] Add person
-    [2] Delete person
-    [3] Edit person details
-    [4] Print all persons
-    [5] Search for person
-    [6] Exit`);
-
+//Allow the user to implement different searchs on the Egron.
+function searchForPerson() {
+    let choise = 0
+    while (choise != 6) {
+        choise = prompt(`Choose one of the options:
+        [1] Search by name
+        [2] Search by ID number
+        [3] Search by age
+        [4] Search by city
+        [5] Search for one of the options:
+            A. Born in even month
+            B. Has at least two childrens
+            C. Person name or his childrens is a palindrom
+        [6] Exit`);
+    }
     switch (choise) {
         case '1':
-            addNewPerson();
+            searchByName(prompt('Please enter name:'))
             break;
         case '2':
-            deletePerson();
+            searchByID(prompt('Please enter ID:'))
             break;
         case '3':
-            setPerson();
+            searchByAge(prompt('Please enter age:'))
             break;
         case '4':
-            printEgron();
+            searchByCity(prompt('Please enter city:'))
             break;
         case '5':
-            searchForPerson();
-            break;
-        case '7':
-            addNewPerson();
+            searchBySomething();
             break;
     }
 }
+
+let choise = 0;
+
+// while (choise != 10) {
+//     choise = prompt(`Welcome to the Egron, Please choose what you want to do:
+//     [1] Add person
+//     [2] Delete person
+//     [3] Edit person details
+//     [4] Print all persons
+//     [5] Search for person
+//     [6] Exit`);
+
+//     switch (choise) {
+//         case '1':
+//             addNewPerson();
+//             break;
+//         case '2':
+//             deletePerson();
+//             break;
+//         case '3':
+//             setPerson();
+//             break;
+//         case '4':
+//             printEgron();
+//             break;
+//         case '5':
+//             searchForPerson();
+//             break;
+//         case '7':
+//             addNewPerson();
+//             break;
+//     }
+// }
