907185a8d26413ff3d41ef2bdf882fc59780822d
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2021-11-07 00:05:03 +0200

    update

--- Egron/functions.js
+++ Egron/functions.js
@@ -1,4 +1,6 @@
 const tbody = document.querySelector('tbody');
+
+const keys = ['fName','lName', 'ID', 'city', 'birthDate', 'parentID'];
 let persons = [
   {
     fName: "Daniel Eitan",
@@ -6,7 +8,7 @@ let persons = [
     ID: "318448032",
     city: "Jerusalem",
     birthDate: "07/06/1997",
-    ParentID: "318448033"
+    parentID: "318448033"
   },
   {
     fName: "Michael",
@@ -14,7 +16,7 @@ let persons = [
     ID: "218432793",
     city: "Bnei Brak",
     birthDate: "21/05/1996",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Yosef",
@@ -22,7 +24,7 @@ let persons = [
     ID: "20944657",
     city: "Beitar",
     birthDate: "06/08/1997",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Shira",
@@ -30,7 +32,7 @@ let persons = [
     ID: "209349299",
     city: "Jerusalem",
     birthDate: "26/12/1997",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "David",
@@ -38,7 +40,7 @@ let persons = [
     ID: "33776543",
     city: "Beit Shemesh",
     birthDate: "12/03/1992",
-    ParentID: "22845312"
+    parentID: "22845312"
   },
   {
     fName: "Eldad",
@@ -46,7 +48,7 @@ let persons = [
     ID: "3314654",
     city: "Jerusalem",
     birthDate: "09/07/1998",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Yosef",
@@ -54,7 +56,7 @@ let persons = [
     ID: "3297887",
     city: "Jerusalem",
     birthDate: "03/12/1992",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Avraham",
@@ -62,7 +64,7 @@ let persons = [
     ID: "5432987",
     city: "Jerusalem",
     birthDate: "22/11/1998",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Binyomin",
@@ -70,7 +72,7 @@ let persons = [
     ID: "33195443",
     city: "Jerusalem",
     birthDate: "01/07/1989",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Ezra",
@@ -78,7 +80,7 @@ let persons = [
     ID: "3215494",
     city: "Tel Aviv",
     birthDate: "09/11/1974",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Avigail",
@@ -86,7 +88,7 @@ let persons = [
     ID: "31944883",
     city: "Haifa",
     birthDate: "02/10/1994",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Reuven",
@@ -94,7 +96,7 @@ let persons = [
     ID: "30264245",
     city: "Natanya",
     birthDate: "27/06/1997",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Emanuel",
@@ -102,7 +104,7 @@ let persons = [
     ID: "32148722",
     city: "Jerusalem",
     birthDate: "04/11/1997",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Yonatan",
@@ -110,7 +112,7 @@ let persons = [
     ID: "208642801",
     city: "Petach Tikva",
     birthDate: "18/04/1998",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Zeev",
@@ -118,7 +120,7 @@ let persons = [
     ID: "39228302",
     city: "Jerusalem",
     birthDate: "06/09/1969",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Aviad",
@@ -126,7 +128,7 @@ let persons = [
     ID: "12385308",
     city: "Maale Adumim",
     birthDate: "27/09/1990",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Moshe",
@@ -134,7 +136,7 @@ let persons = [
     ID: "3798294",
     city: "Jerusalem",
     birthDate: "19/07/1992",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Daniel",
@@ -142,7 +144,7 @@ let persons = [
     ID: "12364267",
     city: "Raanana",
     birthDate: "27/05/1997",
-    ParentID: ""
+    parentID: ""
   },
   {
     fName: "Dad",
@@ -150,7 +152,7 @@ let persons = [
     ID: "33664492",
     city: "Jerusalem",
     birthDate: "04/11/1990",
-    ParentID: ""
+    parentID: ""
   }
 ];
 
@@ -182,9 +184,66 @@ function addPersonNew(person) {
 function editPersonNew(event) {
   let tr = event.target.parentElement.parentElement;
   let childs = tr.children;
-  for (let index = 0; index < childs.length; index++) {
-    childs[index].innerHTML = `<input type = "text" value = "${childs[index].textContent}"></input>`
+  for (let index = 0; index < childs.length - 1; index++) {
+    if (index != 2 || childs[index].innerText == '') {
+      childs[index].setAttribute('contenteditable', 'true');
+    }
+  }
+  childs[childs.length - 1].firstChild.className = 'fas fa-check edit';
+  let i = document.createElement('i');
+  i.className = 'fas fa-times';
+  i.onclick = (event) => deletePerson(event);
+  childs[childs.length - 1].prepend(i);
+  childs[childs.length - 1].lastChild.onclick = (event) => finishEditPerson(event);
+}
+
+function finishEditPerson(event) {
+  let tr = event.target.parentElement.parentElement;
+  let childs = tr.children;
+  for (let index = 0; index < childs.length - 1; index++) {
+    childs[index].setAttribute('contenteditable', 'false');
+    persons[Array.from(tr.parentNode.children).indexOf(tr)][keys[index]] = childs[index].innerText;
+  }
+  childs[childs.length - 1].removeChild(childs[childs.length - 1].firstChild)
+  childs[childs.length - 1].firstChild.className = 'fas fa-pen edit';
+  childs[childs.length - 1].firstChild.onclick = (event) => editPersonNew(event);
+}
+
+function updateTable() {
+  tbody.innerHTML = '';
+  initialTable();
+}
+
+function addColumn() {
+  let newtr = document.createElement('tr');
+  newtr.innerHTML = '<tr><td></td><td></td><td contentediable = "true"></td><td></td><td></td><td></td><td><i onclick = "editPersonNew(event)" class="fas fa-pen edit"></i></td></tr>';
+  tbody.prepend(newtr);
+  persons.unshift({});
+}
+
+function deletePerson(event) {
+  let tr = event.target.parentElement.parentElement;
+  if (confirm('are you sure you want to delete?')) {
+    persons.splice(Array.from(tr.parentNode.children), 1);
+    tr.remove();
   }
 }
+///////////////////////sorting table functions////////////////////////////////
+function sortByfName() {
+  persons.sort((a, b) => a.fName.toLowerCase() < b.fName.toLowerCase() ? -1 : a.fName.toLowerCase() > b.fName.toLowerCase() ? 1 : 0);
+  updateTable();
+}
+
+function sortBylName() {
+  persons.sort((a, b) => a.lName.toLowerCase() < b.lName.toLowerCase() ? -1 : a.lName.toLowerCase() > b.lName.toLowerCase() ? 1 : 0);
+  updateTable();
+}
+
+function sortByCity() {
+  persons.sort((a, b) => a.city.toLowerCase() < b.city.toLowerCase() ? -1 : a.city.toLowerCase() > b.city.toLowerCase() ? 1 : 0);
+  updateTable();
+}
+
+
 
 initialTable()
