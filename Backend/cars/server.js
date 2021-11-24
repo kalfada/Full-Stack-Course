3539e7532f73dab099ce5d2afd41569f94772376
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2021-11-24 16:11:33 +0200

    update

---
+++ Backend/cars/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/cars', function(req, res){
+    
+})
+
+
+app.listen(3000, () => console.log('server is on'))
---
+++ Backend/express/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/', function (req, res) {
+
+    res.send('hello world')
+})
+
+app.get('/calc', function (req, res) {
+    let action = req.query.operator;
+    let num1 = req.query.num1
+    let num2 = req.query.num2
+
+    switch (action) {
+        case 'sum':
+            res.send(String(Number(num1) + Number(num2)))
+            break;
+        case 'sub':
+            res.send(String(num1 - num2))
+            break;
+        case 'mul':
+            res.send(String(num1 * num2))
+            break;
+        case 'div':
+            res.send(String(num1 / num2))
+            break;
+        default:
+            res.sendStatus(404)
+            break;
+    }
+})
+
+app.post('/calc', function (req, res) {
+    let action = req.body.operator;
+    let num1 = req.body.num1
+    let num2 = req.body.num2
+
+    switch (action) {
+        case 'sum':
+            res.send(String(Number(num1) + Number(num2)))
+            break;
+        case 'sub':
+            res.send(String(num1 - num2))
+            break;
+        case 'mul':
+            res.send(String(num1 * num2))
+            break;
+        case 'div':
+            res.send(String(num1 / num2))
+            break;
+        default:
+            res.sendStatus(404)
+            break;
+    }
+})
+
+
+app.listen(3000, () => console.log('server is running'));
+
