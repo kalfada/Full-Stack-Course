c4cfad816e6ff99e5bae41707cd473f4abffd6f3
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2021-11-28 01:56:34 +0200

    update

--- Backend/cars/server.js
+++ Backend/cars/server.js
@@ -3,30 +3,59 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.static('public'))
+
 
 let cars = [];
+let cnt = 1;
 
-app.get('/', function(req, res){
-    res.send('hello')
+app.post('/cars', function (req, res) {
+    const { body } = req;
+    body.id = cnt++;
+    cars.push(body);
+    res.send('success');
 })
 
-app.get('/cars', function(req, res){
-    res.send(cars)
+//Get car by ID, if there is no id send all cars
+app.get('/cars/:id?', function (req, res) {
+    const { id } = req.params
+    if (id) {
+        const car = cars.find(c => c.id == id);
+        car ? res.send(car) : res.send(`car with id=${id} not exist`);
+    } else {
+        res.send(cars);
+    }
+});
+
+app.put('/cars/:id', function (req, res) {
+    const { body, params } = req;
+    const { id } = params;
+
+    const index = cars.findIndex(c => c.id == id);
+
+    if (index == -1) {
+        res.send(`car with id=${id} not exist`);
+    } else {
+        delete body.id
+        cars[index] = { ...cars[index], ...body }
+        res.send(cars[index])
+    }
 })
 
-app.post('/cars/add', function(req, res){
-    const {company, model, color, year, gear, sunroof} = req.body
-    cars.push({company, model, color, year, gear, sunroof});
-    res.send('saved');
-})
+app.delete('/cars/:id', function (req, res) {
+    const { id } = req.params
 
-app.post('/cars/get', function(req, res){
-    res.send(cars);
-})
+    const index = cars.findIndex(c => c.id == id)
 
-app.delete('/cars/delete', function(req, res){
-    
-})
+    if (index == -1)
+        res.send(`car with id=${id} not exist`)
+    else {
+        const deleted = cars[index]
+        cars.splice(index, 1)
+        res.send(deleted)
+    }
+});
 
 
 
--- Michael/script.js
+++ Michael/script.js
@@ -1,16 +1,30 @@
-function carDetails(company, model, color, year, gear, sunroof) {
-    this.company = company,
-        this.model = model,
-        this.color = color,
-        this.year = year,
-        this.gear = gear,
-        this.sunroof = sunroof
-}
+// function carDetails(company, model, color, year, gear, sunroof) {
+//     this.company = company,
+//         this.model = model,
+//         this.color = color,
+//         this.year = year,
+//         this.gear = gear,
+//         this.sunroof = sunroof
+//     }
 
 let carArr = []
-let car1 = new carDetails('suzuki', 'alto', 'gray', '2011', 'manual', false)
-// let car2 = new carDetails('suzuki', 'alto', 'gray', '2011', 'manual', false)
-function addNewCar(car) {
-    carArr.push(car)
+
+// let car2 = new carDetails('s uzuki', 'alto', 'gray', '2011', 'manual', false)
+carArr.push({ company: 'suzuki', model: 'alto', color: 'gray', year: '2011', gear: 'manual', sunroof: false })
+// console.log(carArr);
+
+
+
+document.querySelector('form').onsubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
 }
-addNewCar(car1)
+
+
+
+
+
+
+// document.querySelector('button').onclick = () =>{
+//     document.getElementById('name').innerText = 'Micheal';
+// }
