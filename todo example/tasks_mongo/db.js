b3c6352ef65c71d7ac6aec91b5b9ef60df38c926
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2021-12-30 23:04:44 +0200

    update .env files

--- todo example/tasks_mongo/db.js
+++ todo example/tasks_mongo/db.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose')
-
-const
-    userName = 'kalfada',
-    pass = 'Mottiemmanuelle123',
-    connectionString = `mongodb+srv://${userName}:${pass}@cluster0.d48l6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-
+const dotenv = require('dotenv')
+dotenv.config()
+const connectionString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.d48l6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    
 mongoose.connect(connectionString)
     .then(res => {
         console.log('mongo connected')
